f4e3ae3ea74800055f8788c22a9dd9be57dacd8c
hajin <kimhajin01@naver.com>
2025-01-07 15:25:08 +0900

    Feat: 초기 세팅 >> >> 2025.01.07 >> 프로젝트 생성 Related to: #1

---
+++ src/main/java/blaybus/happynewyear/HappynewyearApplication.java
@@ -0,0 +1,13 @@
+package blaybus.happynewyear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappynewyearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappynewyearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/blaybus/happynewyear/HappynewyearApplicationTests.java
@@ -0,0 +1,13 @@
+package blaybus.happynewyear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappynewyearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
