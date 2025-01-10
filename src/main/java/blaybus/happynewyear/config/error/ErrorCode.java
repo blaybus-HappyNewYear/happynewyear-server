0d426497f7ad51e7143363fb7fdd864a2bd35f00
hajin <kimhajin01@naver.com>
2025-01-11 06:37:37 +0900

    Fix: 에러 코드 오류 수정

--- src/main/java/blaybus/happynewyear/config/error/ErrorCode.java
+++ src/main/java/blaybus/happynewyear/config/error/ErrorCode.java
@@ -11,13 +11,14 @@ public enum ErrorCode {
     INVALID_CURRENT_PASSWORD(HttpStatus.UNAUTHORIZED, "E401002", "잘못된 패스워드입니다."),
 
     DUPLICATED_USERNAME(HttpStatus.CONFLICT, "E409001", "이미 사용중인 사용자 이름입니다."),
+    DUPLICATED_ID(HttpStatus.CONFLICT, "E409002", "이미 존재하는 사번입니다."),
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "E404001", "존재하지 않는 아이디입니다."),
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "E404002", "존재하지 않는 게시글입니다."),
     QUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "E404003", "존재하지 않는 퀘스트입니다."),
     TEAM_NOT_FOUND(HttpStatus.NOT_FOUND, "E404004", "존재하지 않는 소속입니다."),
 
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"E500001","서버 처리 중 오류가 발생했습니다. 관리자에게 문의하세요.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"E500001","서버 처리 중 오류가 발생했습니다. 관리자에게 문의하세요."),
     NO_NOTIFICATION(HttpStatus.NOT_FOUND,"E404005","사용자 알림이 존재하지 않습니다."),
     NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND,"E404006","요청한 알림이 존재하지 않습니다."),
     INVALID_READ_STATE(HttpStatus.BAD_REQUEST, "E400001","이미 읽은 알람입니다.");
