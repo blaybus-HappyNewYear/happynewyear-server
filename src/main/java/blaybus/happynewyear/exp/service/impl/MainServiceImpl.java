854be19d9c37085c4abb7389c0d9625d9edda229
yunju727 <80828786+yunju727@users.noreply.github.com>
2025-01-14 21:26:52 +0900

    [feat]get cur exp - main

--- src/main/java/blaybus/happynewyear/exp/service/impl/MainServiceImpl.java
+++ src/main/java/blaybus/happynewyear/exp/service/impl/MainServiceImpl.java
@@ -2,12 +2,15 @@ package blaybus.happynewyear.exp.service.impl;
 
 import blaybus.happynewyear.config.error.ErrorCode;
 import blaybus.happynewyear.config.error.exception.BusinessException;
+import blaybus.happynewyear.connector.service.BaseSheetService;
 import blaybus.happynewyear.exp.dto.reponse.AllExpDto;
+import blaybus.happynewyear.exp.dto.reponse.CurrExpDto;
 import blaybus.happynewyear.exp.dto.reponse.RecentExpDto;
 import blaybus.happynewyear.exp.entity.Exp;
 import blaybus.happynewyear.exp.repository.ExpRepository;
 import blaybus.happynewyear.exp.service.MainService;
 import blaybus.happynewyear.member.dto.CharacterDto;
+import blaybus.happynewyear.member.dto.MemberExpDto;
 import blaybus.happynewyear.member.entity.Member;
 import blaybus.happynewyear.member.jwt.JwtTokenProvider;
 import blaybus.happynewyear.member.repository.MemberRepository;
@@ -31,6 +34,7 @@ public class MainServiceImpl implements MainService {
     private final JwtTokenProvider jwtTokenProvider;
     private final MemberRepository memberRepository;
     private final ExpRepository expRepository;
+    private final BaseSheetService baseSheetService;
 
     @Override
     @Transactional
@@ -77,4 +81,43 @@ public class MainServiceImpl implements MainService {
                 .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
         return CharacterDto.toDto(member.getImgNumber());
     }
+
+    @Override
+    @Transactional
+    public CurrExpDto getCurrExp(String accessToken){
+        Claims claims = jwtTokenProvider.parseClaims(accessToken);
+        String username = claims.getSubject();
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        // 시트 데이터 검색조건에 유저 id 넣어주기
+        String sheetName = "예시. 경험치 현황";
+        String rangeToWrite = "B11:B11"; // 경험치 현황시트의 사번 정보가 적혀있는 칸
+        List<List<Object>> idData = List.of(List.of(member.getId().toString()));
+        try{
+            baseSheetService.writeSheetData(sheetName, rangeToWrite, idData);
+        }
+        catch (Exception e) {
+            throw new BusinessException(ErrorCode.SHEET_READ_FAILED);
+        }
+
+        // 유저 id로 세팅한 시트 데이터 값을 가져오기
+        String rangeToRead = "C14:F14";
+        try{
+            List<List<Object>> readSheetData = baseSheetService.readSheetData(sheetName, rangeToRead);
+            if (readSheetData == null || readSheetData.isEmpty() || readSheetData.get(0).size() < 4) throw new BusinessException(ErrorCode.SHEET_READ_FAILED);
+            List<Object> rowData = readSheetData.get(0);
+            int currExp = Integer.parseInt(rowData.get(2).toString());
+
+            return CurrExpDto.builder()
+                    .currExp(currExp)
+                    .build();
+
+        } catch (Exception e) {
+            throw new BusinessException(ErrorCode.SHEET_READ_FAILED, "시트 데이터 읽기 실패: " + e.getMessage());
+        }
+
+    }
+
+
 }
